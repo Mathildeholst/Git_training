1a324c66c58c214656ba101d4c68558a7320a17c
Mathildeholst <math928f@gmail.com>
2024-12-09 12:32:47 +0000

    Resolved again

--- test_packages.R
+++ test_packages.R
@@ -1,6 +1,3 @@
 library(RPostgres)
-<<<<<<< HEAD
-library(tidyverse) #Often used in datascience
-=======
-library(tidyverse) #load many packes
->>>>>>> 5103a7867d4252ff884073f071a76176fcc4ddfa
+library(tidyverse) #load many packes!
+
